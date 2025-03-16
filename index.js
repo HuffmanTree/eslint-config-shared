eaaf36c8823883d8da1c27441302d37408b58559
François Guérin <francoisguerin1@hotmail.fr>
2025-03-16 17:13:23 +0100

    Use `devDependencies` and format `index.js`

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import parser from "@typescript-eslint/parser";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
 import importPlugin from "eslint-plugin-import";
 import js from "@eslint/js";
 import ts from "typescript-eslint";
@@ -11,47 +9,43 @@ export default [
   importPlugin.flatConfigs.recommended,
   importPlugin.flatConfigs.typescript,
   {
-  "settings": {
-    "import/resolver": {
-      "typescript": {}
+    "settings": {
+      "import/resolver": {
+        "typescript": {}
+      }
+    },
+    "plugins": {
+      "@stylistic": stylistic,
+    },
+    "rules": {
+      "no-console": "error",
+      "@stylistic/no-multi-spaces": ["error"],
+      "@typescript-eslint/no-namespace": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { "ignoreRestSiblings": true }
+      ],
+      "@stylistic/semi-spacing": ["error", { "before": false, "after": true }],
+      "eqeqeq": ["error", "always"],
+      "@stylistic/semi": ["error", "always"],
+      "@stylistic/quotes": ["error", "double"],
+      "@stylistic/comma-dangle": ["error", "always-multiline"],
+      "@typescript-eslint/no-explicit-any": "error",
+      "import/no-cycle": "error",
+      "import/order": ["error", { "alphabetize": { "order": "asc", "caseInsensitive": true } }],
+      "@stylistic/indent": ["error", 2, { "SwitchCase": 1 }],
+      "@stylistic/no-trailing-spaces": "error",
+      "@stylistic/no-multiple-empty-lines": ["error", { "max": 1, "maxBOF": 0, "maxEOF": 0 }],
+      "@stylistic/padded-blocks": ["error", "never"],
+      "@typescript-eslint/array-type": ["error", { "default": "generic" }],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        { "selector": ["accessor", "method", "property"], "format": ["camelCase"] },
+        { "selector": ["accessor", "method", "property"], "modifiers": ["private"], "format": ["camelCase"], "leadingUnderscore": "require" },
+        { "selector": "typeLike", "format": ["PascalCase"] },
+        { "selector": "variableLike", "format": ["camelCase"], "leadingUnderscore": "allow" },
+        { "selector": "enumMember", "format": ["UPPER_CASE"] }
+      ]
     }
-  },
-  "languageOptions": {
-    "parser": parser,
-  },
-  "plugins": {
-    "@stylistic": stylistic,
-    "@typescript-eslint": tsPlugin,
-  },
-  "rules": {
-    "no-console": "error",
-    "@stylistic/no-multi-spaces": ["error"],
-    "@typescript-eslint/no-namespace": "warn",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { "ignoreRestSiblings": true }
-    ],
-    "@stylistic/semi-spacing": ["error", { "before": false, "after": true }],
-    "eqeqeq": ["error", "always"],
-    "@stylistic/semi": ["error", "always"],
-    "@stylistic/quotes": ["error", "double"],
-    "@stylistic/comma-dangle": ["error", "always-multiline"],
-    "@typescript-eslint/no-explicit-any": "error",
-    "import/no-cycle": "error",
-    "import/order": ["error", { "alphabetize": { "order": "asc", "caseInsensitive": true } }],
-    "@stylistic/indent": ["error", 2, { "SwitchCase": 1 }],
-    "@stylistic/no-trailing-spaces": "error",
-    "@stylistic/no-multiple-empty-lines": ["error", { "max": 1, "maxBOF": 0, "maxEOF": 0 }],
-    "@stylistic/padded-blocks": ["error", "never"],
-    "@typescript-eslint/array-type": ["error", { "default": "generic" }],
-    "@typescript-eslint/naming-convention": [
-      "error",
-      { "selector": ["accessor", "method", "property"], "format": ["camelCase"] },
-      { "selector": ["accessor", "method", "property"], "modifiers": ["private"], "format": ["camelCase"], "leadingUnderscore": "require" },
-      { "selector": "typeLike", "format": ["PascalCase"] },
-      { "selector": "variableLike", "format": ["camelCase"], "leadingUnderscore": "allow" },
-      { "selector": "enumMember", "format": ["UPPER_CASE"] }
-    ]
-  }
-}];
+  }];
